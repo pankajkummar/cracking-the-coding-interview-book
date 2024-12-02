852f2538035031c157dffc2280f366cc8f9bcc6e
pankaj kumar <iampankajkumar@gmail.com>
2024-12-02 22:27:53 +0530

    initial commit

---
+++ src/main/java/com/coding/cracking_the_coding_interview_book/CrackingTheCodingInterviewBookApplication.java
@@ -0,0 +1,13 @@
+package com.coding.cracking_the_coding_interview_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackingTheCodingInterviewBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrackingTheCodingInterviewBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/cracking_the_coding_interview_book/CrackingTheCodingInterviewBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.cracking_the_coding_interview_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrackingTheCodingInterviewBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
