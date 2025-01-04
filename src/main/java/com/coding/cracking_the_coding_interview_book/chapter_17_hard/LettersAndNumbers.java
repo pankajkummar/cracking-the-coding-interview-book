d95871116a58bccfd4fc24736cbdbd534f6f3c8b
pankajkummar <iampankajkumar001@gmail.com>
2025-01-04 09:59:41 +0530

    day15 letters and numbers optimized

--- src/main/java/com/coding/cracking_the_coding_interview_book/chapter_17_hard/LettersAndNumbers.java
+++ src/main/java/com/coding/cracking_the_coding_interview_book/chapter_17_hard/LettersAndNumbers.java
@@ -1,37 +1,38 @@
 package com.coding.cracking_the_coding_interview_book.chapter_17_hard;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class LettersAndNumbers {
     public static char[] subArray(char[] arr){
-        int startIndex=0;
         int subArrayIndex=0;
         int subArrayLength=0;
+        int sum=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
         for(int i=0; i<arr.length; i++){
-            startIndex=i;
-            int length=0;
-            int sum=0;
-            for(int j=startIndex; j<arr.length; j++){
-                length++;
-                if(Character.isDigit(arr[j])){
-                    sum = sum+1;
+            if(Character.isDigit(arr[i])){
+                sum = sum+1;
 
-                }else{
-                    sum=sum-1;
+            }else{
+                sum=sum-1;
+            }
+            if(map.containsKey(sum)){
+                int indexSum = map.get(sum);
+                if(subArrayLength<(i-indexSum)){
+                    subArrayLength=i-indexSum;
+                    subArrayIndex=indexSum+1;
                 }
-                if(length>subArrayLength && sum==0){
-                    subArrayIndex=startIndex;
-                    subArrayLength=length;
-
-                }
-
+            }
+            else{
+                map.put(sum,i);
             }
         }
         return Arrays.copyOfRange(arr,subArrayIndex,subArrayIndex+subArrayLength);
     }
 
     public static void main(String[] args) {
-        char[] arr= {'a', 'b', '1', '2', 'c', 'd', '3'};
+        char[] arr= {'4','a', '1', '2', 'c', 'd', '3'};
         System.out.println(Arrays.toString(subArray(arr)));
     }
 }
