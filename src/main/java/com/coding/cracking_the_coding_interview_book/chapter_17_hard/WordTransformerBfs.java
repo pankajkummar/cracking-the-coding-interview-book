91c95718121a85b551580cfe94aa82521fafc8de
pankajkummar <iampankajkumar001@gmail.com>
2025-01-17 10:13:25 +0530

    day 27 bfs word tranformer

---
+++ src/main/java/com/coding/cracking_the_coding_interview_book/chapter_17_hard/WordTransformerBfs.java
@@ -0,0 +1,71 @@
+package com.coding.cracking_the_coding_interview_book.chapter_17_hard;
+
+import java.util.*;
+
+public class WordTransformerBfs {
+    public static List<String> transform(String[] words, String input, String output){
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        Map<String,String> previous = new HashMap<>();
+        queue.add(input);
+        previous.put(input, null);
+        visited.add(input);
+        while(!queue.isEmpty()){
+            String current = queue.poll();
+            if(Objects.equals(current, output)){
+                break;
+            }
+            for(int i=0; i<words.length; i++){
+
+                if(isOneWordAway(current, words[i]) && !visited.contains(words[i]))
+                {
+                    queue.add(words[i]);
+                    previous.put(words[i],current);
+                    visited.add(words[i]);
+                }
+
+            }
+
+        }
+
+        return constructPath(previous,input,output);
+    }
+
+    private static List<String> constructPath(Map<String, String> previous, String input, String output) {
+        LinkedList<String> path = new LinkedList<>();
+        String current = output;
+        while(current!=null){
+            path.addFirst(current);
+            current = previous.get(current);
+        }
+        return path;
+    }
+
+    public static boolean isOneWordAway(String word1 , String word2){
+        if(word1.length() != word2.length()){
+            return false;
+        }
+        if(word1 == word2){
+            return false;
+        }
+        int diff =0;
+        for(int i=0; i<word1.length(); i++){
+            if(word1.charAt(i) != word2.charAt(i)){
+                diff++;
+            }
+            if(diff>1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"cat", "bat", "bet", "bot", "bog", "dog"};
+        String input = "cat";
+        String output = "dog";
+
+        List<String> transformation = transform(words, input, output);
+        System.out.println("Tranform path : " + transformation);
+    }
+}
